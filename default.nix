be7afe3cfd3fb2f011e0b4c172e34d3f825dee21
Max Genson <mail@maxgenson.de>
2025-11-19 18:42:21 +0100

    nix: add flake

---
+++ default.nix
@@ -0,0 +1,32 @@
+{
+  system ? builtins.currentSystem,
+}:
+
+let
+  buildEnv = import ./dependencies.nix { inherit system; };
+in
+
+buildEnv.pkgs.rustPlatform.buildRustPackage {
+
+  pname = buildEnv.cargoToml.package.name;
+  version = buildEnv.cargoToml.package.version;
+
+  src = ./.;
+  cargoLock.lockFile = ./Cargo.lock;
+
+  nativeBuildInputs = buildEnv.nativePlatformDeps;
+  buildInputs = buildEnv.platformDeps;
+
+  cargoBuildFlags = [
+    "-p"
+    buildEnv.cargoToml.package.name
+  ];
+
+  doCheck = true;
+
+  meta = with buildEnv.pkgs.lib; {
+    mainProgram = buildEnv.cargoToml.package.name;
+    description = buildEnv.cargoToml.package.description;
+    license = licenses.gpl3Only;
+  };
+}
